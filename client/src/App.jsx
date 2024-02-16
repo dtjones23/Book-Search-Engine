fa82f2970921f5a14312b3940d062600a6b054bd
DeAngelo Jones <deangelojones0708@gmail.com>
2024-02-16 15:55:15 -0600

    added apollo provider to app.jsx and refactored serachbooks.jsx to use mutations

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,30 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
-
+import { ApolloProvider } from '@apollo/react-hooks';
 import Navbar from './components/Navbar';
+
+const client = new ApolloClient({
+  request: operation => {
+    const token = localStorage.getItem('id_token');
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    });
+  },
+  uri: '/graphql'
+});
+
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
+    <ApolloProvider client={client}>
+      <>
+        <Navbar />
+        <Outlet />
+      </>
+    </ApolloProvider>
   );
 }
 
